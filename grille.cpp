64527088858f2ad317bbae4a2b845c75f6e1cf80
Scraick <evd.grude@hotmail.fr>
2015-10-19 16:34:01 +0200

    Probleme lecture fichier

--- grille.cpp
+++ grille.cpp
@@ -1,6 +1,9 @@
 #include "grille.h"
 
+const int Nbligne = 20;
+const int Nbcolonne = 20;
 
+string Matrice[Nbligne][Nbcolonne];
 
 grille::grille()
 {
@@ -9,4 +12,40 @@ grille::grille()
 
 grille::~grille()
 {
+}
+
+void grille::dessinerNiveauBas()
+{
+	ifstream fichier("Matricetest.txt", ios::in);  // on ouvre le fichier en lecture
+
+	if (fichier)  // si l'ouverture a réussi
+	{
+
+	for (int x = 0; x < Nbligne; x++)
+	{
+		for (int y = 0; y < Nbcolonne; y++)
+		{
+		
+			string ligne;
+			while (getline(fichier, ligne, '#'))
+			{
+				Matrice[y][x] = ligne;
+			}	
+		}
+	}
+
+	fichier.close();
+
+	for (int x = 0; x < Nbligne; x++)
+	{
+		for (int y = 0; y < Nbcolonne; y++)
+		{
+			cout << Matrice[y][x];
+		}
+	}
+
+	}
+	else  // sinon
+		cout << "Impossible d'ouvrir le fichier !" << endl;
+
 }
--- grille.h
+++ grille.h
@@ -1,10 +1,24 @@
-#pragma once
+#ifndef GRILLE_H
+#define GRILLE_H
+#include <fstream>
+#include <iostream>
+#include <string>
 
+using namespace std;
 
 class grille
 {
+
+	
+
 public:
 	grille();
 	~grille();
+
+	
+	void dessinerNiveauBas();
 };
 
+extern string Matrice[20][20];
+
+#endif
